4cabd9ee1dda091970afb864826d0d62c061f54f
Taishi Yamamoto <t.0910.y@gmail.com>
2021-11-09 21:55:39 +0900

    できたお

--- frontend/src/components/pages/passwordReset.vue
+++ frontend/src/components/pages/passwordReset.vue
@@ -1,6 +1,7 @@
 <template>
   <v-app>
     <v-container>
+    <v-img class="logo"  :src="require('../../assets/logo2.png')"></v-img>
     <h1 class="password-title">パスワード再設定</h1>
     <p class="password-text">
       パスワード再設定用URLを送信しますので、ご登録いただいたメールアドレスを入力し、送信ボタンを押してください。
@@ -14,23 +15,53 @@
             label="メールアドレス"
             id="email"
             type="email"
+            :rules="emailRules"
             required
           ></v-text-field>
         </v-form>
       </v-card-text>
     </v-card>
-    <v-btn color="primary" class="button">送 信</v-btn>
+    <v-btn rounded large color="primary" class="button" @click="submit">送 信</v-btn>
     </v-container>
   </v-app>
 </template>
 
 <script>
+export default {
+  data: () => ({
+    valid: true,
+    email: null,
+    emailRules: [
+      value => !!value || '未入力です。',
+      value => (value || '').length <= 20 || 'Max 20 characters',
+      value => {
+        const pattern = /^(([^<>()[\]\\.,;:\s@]+(\.[^<>()[\]\\.,;:\s@]+)*)|(.+))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        return pattern.test(value) || 'メールアドレス形式ではありません。'
+      }
+    ]
+  }),
+  mounted () {
+    // エラーをリセット
+    this.$refs.form.resetValidation()
+  },
+  methods: {
+    submit () {
+      if (this.$refs.form.validation()) {
+        // 送信処理
+      }
+    }
+  }
+}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 h1 {
   font-weight: normal;
+}
+.logo{
+  margin: 0 auto;
+  width: 700px;
 }
 .password-title {
   width: auto;
