2628f66a125ee574dc84d641740a792cc8de3663
Yuto Nakano <guitarhythmyuto@gmail.com>
2021-11-09 23:00:37 +0900

    空です

---
+++ frontend/src/components/pages/clientField.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app>
+  </v-app>
+</template>
+
+<script>
+export default {
+  components: {
+  },
+  conputed: {
+  },
+  data: () => ({
+  }),
+  methods: {
+
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
