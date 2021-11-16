4cefe127d9edbce25ca08849123a6fbde8218919
Yuto Nakano <guitarhythmyuto@gmail.com>
2021-11-16 21:33:06 +0900

    row内の上下中央揃え

--- frontend/src/components/pages/clientField.vue
+++ frontend/src/components/pages/clientField.vue
@@ -1,88 +1,49 @@
 <template>
   <v-row justify="center">
-    <v-dialog
-      v-model="dialog"
-      persistent
-      max-width="600px"
-    >
+    <v-dialog v-model="dialog" persistent max-width="600px">
       <template v-slot:activator="{ on, attrs }">
-        <v-btn
-          color="primary"
-          dark
-          v-bind="attrs"
-          v-on="on"
-        >
+        <v-btn color="primary" dark v-bind="attrs" v-on="on">
           Open Dialog
         </v-btn>
       </template>
       <v-card>
         <v-card-title>
           <v-row>
-          <v-col cols=12 lg=12 sm=12 align="center">
-          <span class="text-h5 ">客先追加</span>
-          </v-col>
+            <v-col cols="12" lg="12" sm="12">
+              <span class="text-h5">客先追加</span>
+            </v-col>
           </v-row>
         </v-card-title>
         <v-card-text>
-            <v-row>
-              <v-col
-                cols="12"
-                sm="6"
-                md="6"
-              >
-              <p>客先名</p>
-              </v-col>
-              <v-col
-                cols="12"
-                sm="6"
-                md="6"
-              >
-                <v-text-field
-                  v-model="firstname"
-                  :rules="nameRules"
-                  label="(例)株式会社ABC"
-                  required
-                ></v-text-field>
-              </v-col>
-            </v-row>
-            <v-row>
-              <v-col
-                cols="12"
-                sm="6"
-              >ステータス
-              </v-col>
-              <v-col>
-              <v-radio-group
-               v-model="row"
-               row
-               mandatory
-              >
-              <v-radio
-                 label="進行中"
-                 value="radio-1"
-                 ></v-radio>
-                 <v-radio
-                 label="未進行"
-                 value="radio-2"
-               ></v-radio>
-               </v-radio-group>
-              </v-col>
-            </v-row>
+          <v-row align="center">
+            <v-col cols="12" sm="6" md="6">
+              客先名
+            </v-col>
+            <v-col cols="12" sm="6" md="6">
+              <v-text-field
+                v-model="firstname"
+                :rules="nameRules"
+                label="(例)株式会社ABC"
+                required
+              ></v-text-field>
+            </v-col>
+          </v-row>
+          <v-row align="center">
+            <v-col cols="12" sm="6">ステータス </v-col>
+            <v-col>
+              <v-radio-group v-model="row" row mandatory>
+                <v-radio label="進行中" value="radio-1"></v-radio>
+                <v-radio label="未進行" value="radio-2"></v-radio>
+              </v-radio-group>
+            </v-col>
+          </v-row>
         </v-card-text>
         <v-card-actions>
           <v-spacer></v-spacer>
-          <v-btn
-            color="blue darken-1"
-            text
-            @click="dialog = false"
-          >
+          <v-btn color="blue darken-1" text @click="dialog = false">
             確定
           </v-btn>
-          <v-btn
-            color="blue darken-1"
-            text
-            @click="dialog = false"
-          >
+          <v-btn color="blue darken-1" text @click="dialog = false">
             キャンセル
           </v-btn>
         </v-card-actions>
@@ -112,5 +73,4 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
 </style>
