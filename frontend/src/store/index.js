7217ba36aeb25a1187aebad2591e9ddc373d5978
Yuto Nakano <guitarhythmyuto@gmail.com>
2021-12-14 22:34:16 +0900

    Vuexにてデータを取得

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -2,13 +2,15 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import counter from './modules/counter/index'
 import attendanceList from './modules/attendanceList/index'
+import contactList from './modules/contactList/index'
 
 Vue.use(Vuex)
 export default new Vuex.Store({
   strict: true,
   modules: {
     counter,
-    attendanceList
+    attendanceList,
+    contactList
   },
   state: {
   },
