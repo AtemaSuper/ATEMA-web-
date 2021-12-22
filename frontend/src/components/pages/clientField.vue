e37c83a656e3b9c2fc2a1a1352c2cbcb89d7c60a
vivid-kzm7 <kazu02biz@example.com>
2021-12-22 19:13:39 +0900

    コメントアウト

--- frontend/src/components/pages/clientField.vue
+++ frontend/src/components/pages/clientField.vue
@@ -1,212 +1,123 @@
 <template>
-  <v-app id="inspire">
-    <v-row class="justify-start">
-      <v-col cols="1">基本情報</v-col>
-    </v-row>
-    <v-row>
-      <v-col cols="2" class="item-label">会社名</v-col>
-      <v-col cols="3">
-        <v-text-field
-          outlined
-          :rules="[rules.required]"
-          name="input-10-2"
-        ></v-text-field>
-      </v-col>
-    </v-row>
-    <v-row>
-      <v-col cols="2" class="item-label">設立</v-col>
-      <v-col cols="1">
-        <v-text-field outlined label="年"></v-text-field>
-      </v-col>
-      <v-col cols="1">
-        <v-text-field outlined label="月"></v-text-field>
-      </v-col>
-      <v-col cols="1">
-        <v-text-field outlined label="日"></v-text-field>
-      </v-col>
-    </v-row>
-    <v-row>
-      <v-col cols="2" class="item-label">代表者</v-col>
-      <v-col cols="3">
-        <v-text-field outlined></v-text-field>
-      </v-col>
-    </v-row>
-    <v-row>
-      <v-col cols="2" class="item-label">郵便番号</v-col>
-      <v-col cols="1">
-        <v-text-field outlined></v-text-field>
-      </v-col>
-      <v-col cols="2">
-        <v-text-field outlined></v-text-field>
-      </v-col>
-    </v-row>
-    <v-row>
-      <v-col cols="2" class="item-label">住所</v-col>
-      <v-col cols="3">
-        <v-text-field outlined></v-text-field>
-      </v-col>
-    </v-row>
-    <v-row>
-      <v-col cols="2" class="item-label">電話番号</v-col>
-      <v-col cols="1">
-        <v-text-field outlined></v-text-field>
-      </v-col>
-      <v-col cols="1">
-        <v-text-field outlined></v-text-field>
-      </v-col>
-      <v-col cols="1">
-        <v-text-field outlined></v-text-field>
-      </v-col>
-    </v-row>
-    <v-row>
-      <v-col cols="2" class="item-label">工種</v-col>
-      <v-col cols="3">
-        <v-select :items="items" label="Solo field" solo></v-select>
-        <v-btn class="mx-2" fab dark color="indigo">
-          <v-icon dark> mdi-plus </v-icon>
-        </v-btn>
-      </v-col>
-    </v-row>
-  </v-app>
-</template>
-<!--
-<template>
   <v-row justify="center">
-    <v-dialog v-model="dialog" persistent max-width="600px">
+    <!-- <v-dialog
+      v-model="dialog"
+      persistent
+      max-width="600px"
+    >
       <template v-slot:activator="{ on, attrs }">
-        <v-btn color="primary" dark v-bind="attrs" v-on="on">
-          客先追加ボタン
+        <v-btn
+          color="primary"
+          dark
+          v-bind="attrs"
+          v-on="on"
+        >
+          工事編集ボタン
         </v-btn>
       </template>
       <v-card>
         <v-card-title>
           <v-row>
             <v-col cols="12" lg="12" sm="12">
-              <span class="text-h4">客先追加</span>
+            <span class="text-h4">工事編集</span>
             </v-col>
           </v-row>
         </v-card-title>
         <v-card-text>
-          <v-row align="center">
-            <v-col cols="12" sm="4" md="4">
-              <p class="text-h5">客先名</p>
-            </v-col>
-              <v-col cols="12" class="red--text" lg="2" sm="2">
-                <p>※必須</p>
-              </v-col>
-            <v-col cols="12" sm="6" md="6">
-              <v-text-field
-                v-model="firstname"
-                :rules="nameRules"
-                outlined
-                label="(例)株式会社ABC"
-                required
-              ></v-text-field>
-            </v-col>
-          </v-row>
-          <v-row align="center">
-            <v-col cols="12" sm="4">
-              <p class="text-h5">ステータス</p>
-            </v-col>
-            <v-col cols="12" class="red--text" lg="2" sm="2">
-                <p>※必須</p>
-            </v-col>
-            <v-col>
-              <v-radio-group v-model="row" row mandatory>
-                <v-radio label="進行中" value="radio-1"></v-radio>
-                <v-radio label="未進行" value="radio-2"></v-radio>
-              </v-radio-group>
-            </v-col>
-          </v-row>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn color="primary" @click="dialog = false">
-            確定
-          </v-btn>
-          <v-btn color="white" @click="dialog = false">
-            キャンセル
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
-    <v-dialog v-model="adialog" persistent max-width="600px">
-      <template v-slot:activator="{ on, attrs }">
-        <v-btn color="primary" dark v-bind="attrs" v-on="on">
-          現場編集ボタン
-        </v-btn>
-      </template>
-      <v-card>
-        <v-card-title>
-          <v-row>
-            <v-col cols="12" lg="12" sm="12">
-            <span class="text-h4">現場編集</span>
-            </v-col>
-          </v-row>
-        </v-card-title>
-        <v-card-text>
+          <v-container>
             <v-row align="center">
-              <v-col cols="12" sm="4">
-                <p class="text-h5">客先名</p>
+              <v-col cols="12" class="text-h5" lg="4" sm="4">
+                <p>JobNo.</p>
               </v-col>
               <v-col cols="12" class="red--text" lg="2" sm="2">
                 <p>※必須</p>
               </v-col>
-              <v-col cols="12" sm="6">
-                <v-select
-                  :items="['株式会社A', '株式会社B', '株式会社C', '株式会社D']"
-                  outlined
-                  label="客先名を選択してください"
-                  required
-                ></v-select>
+              <v-col cols="12" sm="6" md="6">
+                <v-text-field v-model="JobName" :rules="JobRules" label="(例)21-0001" outlined required></v-text-field>
               </v-col>
             </v-row>
             <v-row align="center">
-              <v-col cols="12" sm="4" md="4">
-                <p class="text-h5">現場名</p>
+              <v-col cols="12" class="text-h5" lg="4" sm="4">
+                  <p>客先名</p>
               </v-col>
               <v-col cols="12" class="red--text" lg="2" sm="2">
                 <p>※必須</p>
               </v-col>
-              <v-col cols="12" sm="6" md="4">
-                <v-text-field v-model="fieldname" :rules="fieldRules" outlined label="現場名を入力してください" required></v-text-field>
+              <v-col cols="12" sm="6" md="6">
+                <v-select outlined label="(例)株式会社ABC" :items="['株式会社A', '株式会社B', '株式会社C', '株式会社D']" required></v-select>
               </v-col>
             </v-row>
-          <v-row align="center">
-            <v-col cols="12" sm="4">
-              <p class="text-h5">ステータス</p>
-            </v-col>
-            <v-col cols="12" class="red--text" lg="2" sm="2">
-              <p>※必須</p>
-            </v-col>
-            <v-col>
-              <v-radio-group v-model="row" row mandatory>
-                <v-radio label="進行中" value="radio-1"></v-radio>
-                <v-radio label="未進行" value="radio-2"></v-radio>
-              </v-radio-group>
-            </v-col>
-          </v-row>
+            <v-row align="center">
+              <v-col cols="12" class="text-h5" lg="4" sm="4">
+                  <p>現場名</p>
+              </v-col>
+              <v-col cols="12" class="red--text" lg="2" sm="2">
+                <p>※必須</p>
+              </v-col>
+              <v-col cols="12" sm="6" md="6">
+                <v-select outlined label="(例)現場名A" :items="['現場名A', '現場名B', '現場名C', '現場名D']" required></v-select>
+              </v-col>
+            </v-row>
+            <v-row align="center">
+              <v-col cols="12" class="text-h5" lg="4" sm="4">
+                  <p>工事件名</p>
+              </v-col>
+              <v-col cols="12" class="red--text" lg="2" sm="2">
+                <p>※必須</p>
+              </v-col>
+              <v-col cols="12" sm="6" md="6">
+                <v-text-field v-model="constructionName" :rules="constructionRules" label="(例)工事件名１－ABC" outlined required></v-text-field>
+              </v-col>
+            </v-row>
+            <v-row align="center">
+              <v-col cols="12" lg="4" sm="4">
+                <p class="text-h5">ステータス</p>
+              </v-col>
+              <v-col cols="12" class="red--text" lg="2" sm="2">
+                <p>※必須</p>
+              </v-col>
+              <v-col>
+                <v-radio-group v-model="row" row mandatory>
+                  <v-radio label="進行中" value="radio-1"></v-radio>
+                  <v-radio label="完了済" value="radio-2"></v-radio>
+                </v-radio-group>
+              </v-col>
+            </v-row>
+            <v-row align="center">
+              <v-col cols="12" lg="4" sm="4">
+                <p class="text-h5">契約形態</p>
+              </v-col>
+              <v-col cols="12" class="red--text" lg="2" sm="2">
+                <p>※必須</p>
+              </v-col>
+              <v-col>
+                <v-radio-group v-model="row" row mandatory>
+                  <v-radio label="常用" value="radio-1"></v-radio>
+                  <v-radio label="請負" value="radio-2"></v-radio>
+                </v-radio-group>
+              </v-col>
+            </v-row>
+          </v-container>
         </v-card-text>
         <v-card-actions>
           <v-spacer></v-spacer>
           <v-btn
             color="primary"
-            @click="adialog = false"
+            @click="dialog = false"
           >
             確定
           </v-btn>
           <v-btn
             color="white"
-            @click="adialog = false"
+            @click="dialog = false"
           >
             キャンセル
           </v-btn>
         </v-card-actions>
       </v-card>
-    </v-dialog>
+    </v-dialog> -->
   </v-row>
 </template>
--->
 
 <script>
 export default {
@@ -216,15 +127,13 @@ export default {
   },
   data: () => ({
     dialog: false,
-    adialog: false,
-    valid: false,
-    firstname: '',
-    nameRules: [
-      v => !!v || '会社名が未入力です。'
+    JobName: '',
+    JobRules: [
+      v => !!v || 'JobNo.が未入力です。'
     ],
-    fieldname: '',
-    fieldRules: [
-      v => !!v || '現場名が未入力です。'
+    constructionName: '',
+    constructionRules: [
+      v => !!v || '工事件名が未入力です。'
     ]
   }),
   methods: {
@@ -233,5 +142,3 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-</style>
