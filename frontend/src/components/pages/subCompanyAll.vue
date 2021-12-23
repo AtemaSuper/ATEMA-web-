dc15e73bca17d38c5f0e363964c213bd3aa19e1f
Taishi Yamamoto <t.0910.y@gmail.com>
2021-12-23 23:39:46 +0900

    noteContents

--- frontend/src/components/pages/subCompanyAll.vue
+++ frontend/src/components/pages/subCompanyAll.vue
@@ -1,6 +1,6 @@
 <template>
   <v-app id="attendanceManage">
-      <v-container>
+      <v-container class="container-1">
         <v-sheet color="white" rounded outlined>
           <v-row align="center" >
             <v-col cols="12" sm="4" md="2">
@@ -47,211 +47,21 @@
               <template v-slot:[`body.append`]> </template>
 
               <!-- companyName Row -->
-              <template v-slot:[`item.companyName`]="{ item }">
-                <v-edit-dialog
-                  :return-value.sync="item.companyName"
-                  large
-                  @save="setItem(item.companyName)"
-                >
-                  <v-chip :color="getColor(item.companyName)" dark>
-                    {{ item.companyName }}
-                  </v-chip>
-                  <template v-slot:input>
-                    <div class="mt-4 text-h6">ステータスを変更</div>
-                    <v-select
-                      v-model="item.companyName"
-                      :items="companyNameItems"
-                      label="Outlined style"
-                      outlined
-                      autofocus
-                    ></v-select>
-                  </template>
-                </v-edit-dialog>
-              </template>
-
-              <!-- type Row -->
-              <template v-slot:[`item.type`]="{ item }">
-                <v-btn outlined  @click="typeContents(item)">
-                      {{ item.type }}
-                    </v-btn>
-              </template>
-
-              <!-- contract Row -->
-              <template >
-              </template>
-
-              <!-- startTime Row -->
-              <template v-slot:[`item.start`]="{ item }">
-                <v-edit-dialog
-                  :return-value.sync="item.start"
-                  large
-                  @save="setItem(item.start)"
-                >
-                <v-chip
-                label
-                outlined
-                >{{ item.start }}
-                </v-chip>
-                  <template v-slot:input>
-                    <vue-timepicker
-                     format="HH:mm"
-                     manual-input
-                     hide-dropdown
-                     v-model="item.start">
-                     </vue-timepicker>
-                  </template>
-                </v-edit-dialog>
-              </template>
-
-            <!-- restStart Row -->
-              <template v-slot:[`item.restStart`]="{ item }">
-                <v-edit-dialog
-                  :return-value.sync="item.restStart"
-                  large
-                  @save="setItem(item.restStart)"
-                >
-                <v-chip
-                label
-                outlined
-                >{{ item.restStart }}
-                </v-chip>
-                  <template v-slot:input>
-                    <vue-timepicker
-                     format="HH:mm"
-                     manual-input
-                     hide-dropdown
-                     v-model="item.restStart">
-                     </vue-timepicker>
-                  </template>
-                </v-edit-dialog>
-              </template>
-
-              <!-- restEnd Row -->
-              <template v-slot:[`item.restEnd`]="{ item }">
-                <v-edit-dialog
-                  :return-value.sync="item.restEnd"
-                  large
-                  @save="setItem(item.restEnd)"
-                >
-                <v-chip
-                label
-                outlined
-                >{{ item.restEnd }}
-                </v-chip>
-                  <template v-slot:input>
-                   <vue-timepicker
-                     format="HH:mm"
-                     manual-input
-                     hide-dropdown
-                     v-model="item.restEnd">
-                     </vue-timepicker>
-                  </template>
-                </v-edit-dialog>
-              </template>
-
-              <!-- end Row -->
-              <template v-slot:[`item.end`]="{ item }">
-                <v-edit-dialog
-                  :return-value.sync="item.end"
-                  large
-                  @save="setItem(item.end)"
-                >
-                <v-chip
-                label
-                outlined
-                >{{ item.end }}
-                </v-chip>
-                  <template v-slot:input>
-                    <vue-timepicker
-                     format="HH:mm"
-                     manual-input
-                     hide-dropdown
-                     v-model="item.end">
-                     </vue-timepicker>
-                  </template>
-                </v-edit-dialog>
-              </template>
-              <!-- noteContents Row -->
-              <template v-slot:[`item.note`]="{ item }" >
-                <v-btn outlined @click="noteContents(item)">
-                      {{ item.note }}
-                    </v-btn>
+              <template v-slot:[`item.delete`]="{ item }">
+          <v-btn
+            color="error"
+            rounded
+            depressed
+            @click="deleteItem(item)"
+          >
+           <v-icon dark>
+            mdi-delete
+           </v-icon>
+          </v-btn>
               </template>
             </v-data-table>
               </v-row>
         </v-sheet>
-        <!-- noteContentsDialog-->
-        <v-dialog v-model="noteContentsDialog" max-width="300">
-           <v-card>
-                    <v-card-title class="text-h5 grey lighten-2">
-                      備考欄
-                    </v-card-title>
-                    <v-card-text>
-                      <v-textarea
-                      v-model="ditaileEdit.noteContents"
-                      outlined
-                      name="input-7-4"
-                      value= ditaileEdit.noteContents
-                      ></v-textarea>
-                    </v-card-text>
-                    <v-divider></v-divider>
-                    <v-card-actions>
-                      <v-spacer></v-spacer>
-                    </v-card-actions>
-                  </v-card>
-              </v-dialog>
-<!--fieldDialog-->
-              <v-dialog v-model="fieldDialog" max-width="300">
-                  <v-card>
-                    <v-card-title class="text-h5 grey lighten-2">
-                      現場詳細表示
-                    </v-card-title>
-
-                    <v-card-text>
-                      現場名以外内容はベタ書きです
-                      <v-row>
-                        <v-col cols="12" sm="4" md="4" align="right">
-                          <h4>jobNo:</h4>
-                        </v-col>
-                        <v-col cols="12" sm="8" md="8" align="left">
-                          <h4>21-1234</h4>
-                        </v-col>
-                      </v-row>
-                      <v-row>
-                        <v-col cols="12" sm="4" md="4" align="right">
-                          <h4>客先:</h4>
-                        </v-col>
-                        <v-col cols="12" sm="8" md="8" align="left">
-                          <h4>株式会社ABC運送</h4>
-                        </v-col>
-                      </v-row>
-                      <v-row>
-                        <v-col cols="12" sm="4" md="4" align="right">
-                          <h4>現場名:</h4>
-                        </v-col>
-                        <v-col cols="12" sm="8" md="8" align="left">
-                          <h4>{{ ditaileEdit.field }}</h4>
-                        </v-col>
-                      </v-row>
-                      <v-row>
-                        <v-col cols="12" sm="4" md="4" align="right">
-                          <h4>工事件名:</h4>
-                        </v-col>
-                        <v-col cols="12" sm="8" md="8" align="left">
-                          <h4>
-                            ダクト修繕・改築工事ダクト修繕・改築工事ダクト修繕・改築工事
-                          </h4>
-                        </v-col>
-                      </v-row>
-                    </v-card-text>
-
-                    <v-divider></v-divider>
-
-                    <v-card-actions>
-                      <v-spacer></v-spacer>
-                    </v-card-actions>
-                  </v-card>
-                </v-dialog>
       </v-container>
       <v-row class="title">
           <v-col cols="2"></v-col>
@@ -260,11 +70,10 @@
           </v-col>
           <v-col cols="1"></v-col>
         </v-row>
-        <v-container>
+        <v-container class="container-2">
             <v-sheet color="white" rounded outlined>
           <v-row align="center" >
-            <v-col cols="12" sm="4" md="2">
-            </v-col>
+            <v-col cols="12" sm="4" md="2"> </v-col>
             <v-col cols="12" sm="4" md="4"> </v-col>
             <v-col cols="12" sm="4" md="4"> </v-col>
             <v-col cols="12" sm="4" md="2">
@@ -289,8 +98,8 @@
               rowsPerPage: All
               height= '300'
             >
-              <template v-slot:top>
-            <v-row >
+        <template v-slot:top>
+            <v-row>
                 <v-col cols='4'>
             <!-- 検索窓 -->
               <v-text-field
@@ -307,179 +116,34 @@
               <template v-slot:[`body.append`]> </template>
 
               <!-- status Row -->
-              <template v-slot:[`item.status`]="{ item }">
-                <v-edit-dialog
-                  :return-value.sync="item.status"
-                  large
-                  @save="setItem(item.status)"
-                >
-                  <v-chip :color="getColor(item.status)" dark>
-                    {{ item.status }}
-                  </v-chip>
-                  <template v-slot:input>
-                    <div class="mt-4 text-h6">ステータスを変更</div>
-                    <v-select
-                      v-model="item.status"
-                      :items="statusItems"
-                      label="Outlined style"
-                      outlined
-                      autofocus
-                    ></v-select>
-                  </template>
-                </v-edit-dialog>
-              </template>
-
-              <!-- field Row -->
-              <template v-slot:[`item.field`]="{ item }">
-                <v-btn outlined  @click="fieldContents(item)">
-                      {{ item.field }}
-                    </v-btn>
-              </template>
-
-              <!-- contract Row -->
-              <template v-slot:[`item.contract`]="{ item }">
-                <v-edit-dialog
-                  :return-value.sync="item.contract"
-                  large
-                  @save="setItem(item.contract)"
-                >
-                  <v-chip>
-                    {{ item.contract }}
-                  </v-chip>
-                  <template v-slot:input>
-                    <div class="mt-4 text-h6">請負形式を選択</div>
-                    <v-select
-                      v-model="item.contract"
-                      :items="contractItems"
-                      label="Outlined style"
-                      outlined
-                      autofocus
-                    ></v-select>
-                  </template>
-                </v-edit-dialog>
-              </template>
-
-              <!-- startTime Row -->
-              <template v-slot:[`item.start`]="{ item }">
-                <v-edit-dialog
-                  :return-value.sync="item.start"
-                  large
-                  @save="setItem(item.start)"
-                >
-                <v-chip
-                label
-                outlined
-                >{{ item.start }}
-                </v-chip>
-                  <template v-slot:input>
-                    <vue-timepicker
-                     format="HH:mm"
-                     manual-input
-                     hide-dropdown
-                     v-model="item.start">
-                     </vue-timepicker>
-                  </template>
-                </v-edit-dialog>
-              </template>
-
-            <!-- restStart Row -->
-              <template v-slot:[`item.restStart`]="{ item }">
-                <v-edit-dialog
-                  :return-value.sync="item.restStart"
-                  large
-                  @save="setItem(item.restStart)"
-                >
-                <v-chip
-                label
-                outlined
-                >{{ item.restStart }}
-                </v-chip>
-                  <template v-slot:input>
-                    <vue-timepicker
-                     format="HH:mm"
-                     manual-input
-                     hide-dropdown
-                     v-model="item.restStart">
-                     </vue-timepicker>
-                  </template>
-                </v-edit-dialog>
-              </template>
-
-              <!-- restEnd Row -->
-              <template v-slot:[`item.restEnd`]="{ item }">
-                <v-edit-dialog
-                  :return-value.sync="item.restEnd"
-                  large
-                  @save="setItem(item.restEnd)"
-                >
-                <v-chip
-                label
-                outlined
-                >{{ item.restEnd }}
-                </v-chip>
-                  <template v-slot:input>
-                   <vue-timepicker
-                     format="HH:mm"
-                     manual-input
-                     hide-dropdown
-                     v-model="item.restEnd">
-                     </vue-timepicker>
-                  </template>
-                </v-edit-dialog>
-              </template>
-
-              <!-- end Row -->
-              <template v-slot:[`item.end`]="{ item }">
-                <v-edit-dialog
-                  :return-value.sync="item.end"
-                  large
-                  @save="setItem(item.end)"
-                >
-                <v-chip
-                label
-                outlined
-                >{{ item.end }}
-                </v-chip>
-                  <template v-slot:input>
-                    <vue-timepicker
-                     format="HH:mm"
-                     manual-input
-                     hide-dropdown
-                     v-model="item.end">
-                     </vue-timepicker>
-                  </template>
-                </v-edit-dialog>
-              </template>
-              <!-- noteContents Row -->
-              <template v-slot:[`item.note`]="{ item }" >
-                <v-btn outlined @click="noteContents(item)">
-                      {{ item.note }}
-                    </v-btn>
+              <template v-slot:[`item.delete`]="{ item }">
+          <v-btn
+            color="error"
+            depressed
+            rounded
+            @click="deleteItem(item)"
+          >
+           <v-icon dark>
+            mdi-delete
+           </v-icon>
+          </v-btn>
               </template>
             </v-data-table>
-              </v-row>
+          </v-row>
         </v-sheet>
       </v-container>
   </v-app>
 </template>
 
 <script>
-/** VueTimepicker（時刻選択用ライブラリ） */
-import VueTimepicker from 'vue2-timepicker'
-import 'vue2-timepicker/dist/VueTimepicker.css'
-
 export default {
   name: 'attendanceManage',
-  components: {
-    VueTimepicker
-  },
   data: () => ({
     search: '',
     status: '',
     menu: false,
     date: '',
     statusItems: ['出勤中', '休憩中', '退勤中', '早出', '深夜'],
-    contractItems: ['請負', '常用'],
     ditaileEdit: {},
     noteContentsDialog: false,
     fieldDialog: false
@@ -507,6 +171,7 @@ export default {
           width: '30%'
         },
         { text: '削除',
+          value: 'delete',
           sortable: false,
           align: 'right',
           width: '50%'}
@@ -533,7 +198,7 @@ export default {
           },
           width: '30%'
         },
-        { text: '削除', value: 'note', sortable: false, align: 'right', width: '50%' }
+        { text: '削除', value: 'delete', sortable: false, align: 'right', width: '50%' }
       ]
     },
     /** Vuex storeで設定した値を取得 (オブジェクトで取得するので、配列を指名して)リターン */
@@ -574,10 +239,6 @@ export default {
       /** 1-NCMBのデータ更新処理呼び出し express側定義 axioメソッド */
       console.log(item)
     },
-    noteContents (item) {
-      this.ditaileEdit = item
-      this.noteContentsDialog = true
-    },
     fieldContents (item) {
       this.ditaileEdit = item
       this.fieldDialog = true
@@ -594,11 +255,17 @@ export default {
 <style scoped>
 .title {
   height: 75px;
-  padding-top: 25px;
+  padding-top: 12px;
   font-size: 3.8rem;
   background-color: #cccccc;
 }
 .table{
     width: 100%;
+}
+.container-1{
+  padding: 90px;
+}
+.container-2{
+  padding: 90px;
 }
 </style>
