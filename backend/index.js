7df1aa6dd844b4e8cb954b8dd7ec61179a1d4bb0
Yuto Nakano <guitarhythmyuto@gmail.com>
2022-01-21 01:19:53 +0900

    apiモジュールの分割

--- backend/index.js
+++ backend/index.js
@@ -2,15 +2,17 @@ const express = require('express')
 const bodyParser = require('body-parser')
 // corsポリシーに抵触するため、その対策としてcorsを利用する
 const cors = require('cors')
+const test = require('./api/test')
 
 const app = express()
 app.use(bodyParser.json())
 app.use(cors())
+app.use('/test',test)
 
-app.post('/test', function(req, res) {
+/**app.post('/test', function(req, res) {
   res.send({
     message: req.body.text
   })
-})
+})*/
 
 app.listen(process.env.PORT || 3000)
