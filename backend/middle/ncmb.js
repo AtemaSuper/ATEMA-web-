a239672190dda2b37f804bc3660f4b53105b3508
vivid-kzm7 <kazu02biz@example.com>
2022-02-11 21:19:45 +0900

    ncmb

---
+++ backend/middle/ncmb.js
@@ -0,0 +1,4 @@
+var NCMB   = require('ncmb');
+var application_key = "c693810d5d59983dad54088877981edd272984c7eb6cf7ddfdb3feb407425f13";
+var client_key = "34d5ccb39527f3aef7db0048f4b1fad96ee89a4ec503e7ec35bbdeb06bd68dfd";
+var ncmb = new NCMB(application_key, client_key);
