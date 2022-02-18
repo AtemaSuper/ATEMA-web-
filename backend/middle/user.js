260ed1b9fb074e436929cb34c06acca505295738
Yuto Nakano <guitarhythmyuto@gmail.com>
2022-02-18 21:18:57 +0900

    キーファイルの作成

--- backend/middle/user.js
+++ backend/middle/user.js
@@ -2,9 +2,8 @@ const express = require('express')
 const app = express()
 var jwt = require('jsonwebtoken');
 var NCMB   = require('ncmb');
-var application_key = "c693810d5d59983dad54088877981edd272984c7eb6cf7ddfdb3feb407425f13";
-var client_key = "34d5ccb39527f3aef7db0048f4b1fad96ee89a4ec503e7ec35bbdeb06bd68dfd";
-var ncmb = new NCMB(application_key, client_key);
+const ncmb_key = require('../ncmb-key');  
+var ncmb = new NCMB(ncmb_key.application_key, ncmb_key.client_key);
 const cookieParser = require("cookie-parser");
 app.use(cookieParser());
 
