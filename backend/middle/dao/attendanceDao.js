38e0d07b5ef037e435231039a06f3a062a55798a
vivid-kzm7 <kazu02biz@example.com>
2022-02-27 16:49:01 +0900

    daoファイル作成

---
+++ backend/middle/dao/attendanceDao.js
@@ -0,0 +1,33 @@
+"use strict"
+
+var NCMB = require('ncmb');
+let NCMB_KEY = require('../../ncmb-key')
+var ncmb = new NCMB(NCMB_KEY.application_key,NCMB_KEY.client_key);
+
+
+/**
+ * 出退勤テーブルのDaoクラスです。
+ */
+class AttendanceDao {
+  /**
+   * ログインIDをもとに従業員を取得します。
+   * 
+   * @param {string} loginId 
+   * 
+   * @returns
+   */
+  selectEmployeeByloginId(req, res) {
+      var Item = ncmb.DataStore('employeeTable');
+      Item.fetchAll()
+            .then(function(items){
+              res.status(200)
+                  .json(items);
+            })
+            .catch(function(err){
+              res.status(500)
+                  .json(err);
+      });
+  }
+};
+
+module.exports = AttendanceDao;
