33569b8366b4e08e90081e2a07fc782f5f26b953
Yuto Nakano <guitarhythmyuto@gmail.com>
2022-12-28 19:15:18 +0900

    :recycle: ログイン画面のデザインを修正

--- frontend/src/components/pages/login.vue
+++ frontend/src/components/pages/login.vue
@@ -1,72 +1,65 @@
 <template>
   <v-app id="login">
-    <v-row align="center">
-      <v-col sm="5" md="6" class="bg"> </v-col>
-      <v-col sm="5" offset-sm="2" md="6" offset-md="0"
-        ><v-img
-          max-height="150"
-          max-width="400"
-          :src="require('../../assets/logo2.png')"
-          :class="['mx-auto', 'ma-5']"
-        ></v-img>
-        <v-card class="mx-auto" max-width="600" outlined>
-          <v-card-text>
-            <v-card-title primary-title class="justify-center">
-              <div>
-                <h2
-                  class="headline font-weight-bold orange--text text--accent-2"
-                >
-                  契約者用ログイン
-                </h2>
-              </div>
-            </v-card-title>
-            <v-form ref="loginForm">
-              <v-text-field
-                name="login"
-                label="ID"
-                type="text"
-                v-model="model.email"
-                :rules="emailRules"
-                maxlength="50"
-                clearable
-                clear-icon="mdi-close-circle"
-                outlined
-                required
-              ></v-text-field>
-              <v-text-field
-                name="password"
-                label="パスワード"
-                id="password"
-                type="password"
-                v-model="model.password"
-                :rules="passwordRules"
-                maxlength="50"
-                clearable
-                clear-icon="mdi-close-circle"
-                outlined
-                required
-              >
-              </v-text-field>
-            </v-form>
-            <!-- <v-btn block color="primary" @click="login" :loading="loading">ログイン</v-btn> -->
-            <v-card-actions class="justify-center">
-              <v-btn block color="primary" @click="login()">ログイン</v-btn>
-            </v-card-actions>
-            <v-spacer />
-            <v-card-actions class="justify-center">
-              <v-checkbox
-                v-model="checkbox"
-                label="ログイン状態を保持しますか？"
-              ></v-checkbox>
-            </v-card-actions>
-            <router-link to="/passwordReset"
-              >パスワードを忘れた場合</router-link
+    <div>
+      <v-img
+        max-height="150"
+        max-width="400"
+        :src="require('../../assets/logo2.png')"
+        :class="['mx-auto', 'ma-5']"
+      ></v-img>
+      <v-card class="mx-auto" max-width="600" outlined>
+        <v-card-text>
+          <v-card-title primary-title class="justify-center">
+            <div>
+              <h2 class="headline font-weight-bold text--accent-1">
+                契約者用ログイン
+              </h2>
+            </div>
+          </v-card-title>
+          <v-form ref="loginForm">
+            <v-text-field
+              name="login"
+              label="ID"
+              type="text"
+              v-model="model.email"
+              :rules="emailRules"
+              maxlength="50"
+              clearable
+              clear-icon="mdi-close-circle"
+              outlined
+              required
+            ></v-text-field>
+            <v-text-field
+              name="password"
+              label="パスワード"
+              id="password"
+              type="password"
+              v-model="model.password"
+              :rules="passwordRules"
+              maxlength="50"
+              clearable
+              clear-icon="mdi-close-circle"
+              outlined
+              required
             >
-          </v-card-text>
-        </v-card>
-        <a href="">新規会員登録はこちら</a>
-      </v-col>
-    </v-row>
+            </v-text-field>
+          </v-form>
+          <!-- <v-btn block color="primary" @click="login" :loading="loading">ログイン</v-btn> -->
+          <v-card-actions class="justify-center">
+            <v-btn block color="primary" @click="login()">ログイン</v-btn>
+          </v-card-actions>
+          <v-spacer />
+          <v-card-actions class="justify-center">
+            <v-checkbox
+              v-model="checkbox"
+              label="ログイン状態を保持しますか？"
+            ></v-checkbox>
+          </v-card-actions>
+          <router-link to="/passwordReset">パスワードを忘れた場合</router-link>
+        </v-card-text>
+      </v-card>
+      <a href="">新規会員登録はこちら</a>
+    </div>
   </v-app>
 </template>
 
@@ -119,11 +112,3 @@ export default {
   }
 };
 </script>
-<style scoped>
-.bg {
-  background-image: url("../../assets/bg1.jpg");
-  background-size: cover;
-  background-position: center center;
-  height: 100%;
-}
-</style>
